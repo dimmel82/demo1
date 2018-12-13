0b34f18dfa85be9d32be9607b4b8fd038f44e4a6
dmeletiou <dmeletiou@intrasoft-intl.com>
2018-12-13 13:50:23 +0200

    Initialize git repo

---
+++ src/main/groovy/jx/poc/demo1/DemoApplication.groovy
@@ -0,0 +1,14 @@
+package jx.poc.demo1
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DemoApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run DemoApplication, args
+	}
+
+}
+
